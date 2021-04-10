f86e16093769fde2b648dec2848842f1b2105dc7
shadykh <eng.shady94@gmail.com>
2021-04-10 15:03:58 +0300

    challenge10,Finshed

--- code-challenges/challenges-10.test.js
+++ code-challenges/challenges-10.test.js
@@ -100,7 +100,7 @@ const divisibleByFiveTwoToThePower = (input) => {
     })
     localArray.push(insideArray);
   })
-  return b;
+  return localArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
