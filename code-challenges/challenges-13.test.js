c27495cf952f5632ff3cf5e6cfbe24c80e526c2b
shadykh <eng.shady94@gmail.com>
2021-04-14 14:30:33 +0300

    challenge13,Finshed

--- code-challenges/challenges-13.test.js
+++ code-challenges/challenges-13.test.js
@@ -63,7 +63,7 @@ For example, (123) 456-7890 returns 1234567890
 
 const standardizePhoneNumbers = (arr) => {
   // Solution code here...
-  localArray = [];
+  let localArray = [];
   arr.forEach(element => {
     localArray.push(element.substring(1, 4) + element.substring(6, 9) + element.substring(10, 14))
   });
